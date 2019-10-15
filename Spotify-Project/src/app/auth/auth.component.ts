21e96919d741104174aed9bdef7b77d8cbd41ea9
Darragh McKenna <darragh.mckenna.2018@mumail.ie>
2019-10-15 10:24:25 +0100

    Initial commit of the client side of the project

---
+++ Spotify-Project/src/app/auth/auth.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './auth.service';
+
+@Component({
+    selector: 'pm-auth',
+    templateUrl: 'auth.component.html',
+    styleUrls: ['auth.component.css']
+})
+
+export class AuthComponent implements OnInit{
+
+    constructor(private authService: AuthService){
+
+    }
+
+    ngOnInit(): void {
+        console.log("init");
+    }
+
+    testAuth() : void {
+        console.log("clicked");
+        console.log(this.authService.authorize());
+    }
+
+}
