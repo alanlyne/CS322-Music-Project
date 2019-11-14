45eaaf4388f231935120dede829f2053b9a91d51
alanlyne <alanlyne@outlook.com>
2019-11-14 12:20:26 +0000

    more requirements

--- python_backend/app_launch.py
+++ python_backend/app_launch.py
@@ -3,8 +3,9 @@ import json
 import requests
 import sys
 import spotipy
-
+import os
 from flask import Flask, request
+from spotipy.oauth2 import SpotifyClientCredentials
 
 
 app = Flask(__name__)
@@ -20,30 +21,30 @@ class SpotifyApi:
         """
         search DB
         """
-        return generate_token
+        return generate_token(user_code)
 
-    def generate_token(self, user_code):
-        url="https://accounts.spotify.com/api/token"
-        data={
-            'grant_type': 'client_credentials',
-            'code':user_code,
-            'redirect_uri':"http://localhost:8080/tester"
-        }    
-        client_id = self.client_id
-        client_secret = self.client_secret
+def generate_token(self, user_code):
+    url="https://accounts.spotify.com/api/token"
+    data={
+        'grant_type': 'client_credentials',
+        'code':user_code,
+        'redirect_uri':"http://localhost:8080/tester"
+    }    
+    client_id = self.client_id
+    client_secret = self.client_secret
 
-        response=requests.post(url, data=data, auth = (client_id, client_secret)) 
-        print("-------------------------------------")
-        print(response.status_code)
-        print("-------------------------------------")
+    response=requests.post(url, data=data, auth = (client_id, client_secret)) 
+    print("-------------------------------------")
+    print(response.status_code)
+    print("-------------------------------------")
 
-        return response
+    return response
 
 @app.route('/login')
 def login():    
     if 'code' in request.args:
         code = request.args.get('code')
-        response = SpotifyApi.get_token(code)
+        response = generate_token(code)
         json_response = response.json()
         return json_response
     if 'error' in request.args:
@@ -56,15 +57,28 @@ def login():
 @app.route('/search')
 def search():
 
-    import spotipy
     spotify = spotipy.Spotify()
-    results = spotify.search(q='artist:' + name, type='artist')
-    print results
-    q="genre:Rock" #year:1981
-    results = sp.search(q,limit=10,offset= 9000,type="artist",market="IE")
 
+    q="genre:Rock" #year:1981
+    results = spotify.search('Radiohead')
+    #results = spotify.search(q,limit=10,offset= 9000,type="artist",market="IE")
+    return results
 
 if __name__=='__main__':
+    #SpotifyApi.client_id = sys.argv[1] 
+    #SpotifyApi.client_secret = sys.argv[2] 
+    os.environ['SPOTIPY_CLIENT_ID'] = sys.argv[1]
+    os.environ['SPOTIPY_CLIENT_SECRET'] = sys.argv[2]
+    os.environ['SPOTIPY_REDIRECT_URI'] = 'http://localhost:8080/tester'
+
+    """spotipy.client.SpotifyOAuth(    
+        client_id=sys.argv[1],
+        client_secret=sys.argv[2] ,
+        redirect_uri='http://localhost:8080/tester')
+    """
+
+
+    client_credentials_manager = SpotifyClientCredentials()
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
     app.run(host='0.0.0.0', port=8080)
-    SpotifyApi.client_id = sys.argv[1] 
-    SpotifyApi.client_secret = sys.argv[2] 
