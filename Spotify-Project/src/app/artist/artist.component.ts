0d6cfeb5dc202249dbd935abba69871bdbc5a3c6
Andrewk129 <andrewk129@gmail.com>
2019-11-21 14:26:10 +0000

    Have a great day

--- Spotify-Project/src/app/artist/artist.component.ts
+++ Spotify-Project/src/app/artist/artist.component.ts
@@ -8,49 +8,50 @@ import { IArtist } from "./artist";
   styleUrls: ["artist.component.css"]
 })
 export class ArtistComponent implements OnInit {
-  artistList: IArtist[] = [];
-
-  constructor(private artsitService: ArtistService) {}
+  artistList: IArtist[][] = [];
+  artistList2: IArtist[][] = [];
+  
+  constructor(private artistService: ArtistService) {}
   ngOnInit(): void {
     this.artistList[0] = []
   }
 
-  getArtists(): void {
-    let fiveartist = this.artsitService.getArtists(this.artistList[0].length*this.artistList.length);
-    if(this.artistList[0].length == 0){
-      this.artistList[0] = fiveartist
-    }else{
-      this.artistList.push(fiveartist);
-    }
-    console.log(this.artistList);
-    //console.log(this.artistList[0].images[0].url);
+  // getArtists(): void {
+  //   let fiveartist = this.artistService.getArtists(this.artistList[0].length*this.artistList.length);
+  //   if(this.artistList[0].length == 0){
+  //     this.artistList[0] = fiveartist
+  //   }else{
+  //     this.artistList.push(fiveartist);
+  //   }
+  //   console.log(this.artistList);
+  //   //console.log(this.artistList[0].images[0].url);
 
-    // if(this.artistList[0].images[0].url != "") {
-    //   this.imageTest = this.artistList[0].images[2].url;
-    //   //this.artistLink = this.artistList[0].external_urls.spotify;
-    //   this.artistName = this.artistList[0].name;
-    //   console.log(this.imageTest)
-    //   this.imageBoolean = true;
-
-
-    //   this.artistList.forEach(artist => {
-    //     console.log(artist.images.length > 0 ? artist.images[2].url : "no image found")
+  //   // if(this.artistList[0].images[0].url != "") {
+  //   //   this.imageTest = this.artistList[0].images[2].url;
+  //   //   //this.artistLink = this.artistList[0].external_urls.spotify;
+  //   //   this.artistName = this.artistList[0].name;
+  //   //   console.log(this.imageTest)
+  //   //   this.imageBoolean = true;
 
 
-
-    //   });
-
-    //}
+  //   //   this.artistList.forEach(artist => {
+  //   //     console.log(artist.images.length > 0 ? artist.images[2].url : "no image found")
 
 
-    this.artistList.forEach(artists => {
-      artists.forEach(artist =>{
-        this.htmlYouWantToAdd += "name: " + artist.name + "; followers: " + artist.followers.total + "\n"
 
-      })
+  //   //   });
 
-    });
-  }
+  //   //}
+
+
+  //   this.artistList.forEach(artists => {
+  //     artists.forEach(artist =>{
+  //       this.htmlYouWantToAdd += "name: " + artist.name + "; followers: " + artist.followers.total + "\n"
+
+  //     })
+
+  //   });
+  // }
 
 
   // private addHTML() {
@@ -62,24 +63,36 @@ export class ArtistComponent implements OnInit {
 
   search(): void {
     this.artistService.SearchArtist().subscribe(res => {
+      //Array of artist objects 
       this.artistList = res["artists"]["items"];
       console.log(this.artistList);
       this.artistList.forEach(artist =>{
-        console.log(artist.name);
-        this.htmlYouWantToAdd += artist.name + "<br>";
+        while(this.count != 4) {
+          this.artistList2[this.col][this.count] =  artist;
+          this.count++;
+        }
+        if(this.count != 4) {
+          this.count = 0;
+        }
+        console.log(artist);
+        //this.htmlYouWantToAdd += artist.name + "<br>";
       })
       return this.htmlYouWantToAdd;
     });
+}
 
-    increaseMaxCount(): void {
-    this.maxCount += 5;
-  }
+increaseMaxCount(): void {
+  this.maxCount += 5;
+}
 
-  maxCount: number = 5;
-  imageBoolean: boolean = false;
-  imageTest: string = "";
-  artistLink: string = "";
-  artistName: string = "";
-  htmlYouWantToAdd: string = "";
+maxCount: number = 5;
+imageBoolean: boolean = false;
+imageTest: string = "";
+artistLink: string = "";
+artistName: string = "";
+htmlYouWantToAdd: string = "";
+count: number = 1;
+col: number =0;
+
 
 }
