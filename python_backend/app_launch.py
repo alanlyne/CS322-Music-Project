78320bb04bf4c4949c3b3d2efda55c909c86d59f
omar <omarhafdioui@gmail.com>
2019-11-28 17:31:28 +0000

    improvement on search

--- python_backend/app_launch.py
+++ python_backend/app_launch.py
@@ -5,7 +5,7 @@ import sys
 import spotipy
 import os
 from flask import Flask, request
-from spotipy import  oauth2
+from spotipy import oauth2
 import spotipy.util as util
 from flask.json import jsonify
 from flask_cors import CORS
@@ -13,8 +13,8 @@ from flask_cors import CORS
 app = Flask(__name__)
 #cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 CORS(app)
-# client_id = sys.argv[1] 
-# client_secret = sys.argv[2] 
+# client_id = sys.argv[1]
+# client_secret = sys.argv[2]
 redirect_uri = 'http://localhost:8080/tester'
 
 client_id = ''
@@ -23,94 +23,98 @@ os.environ['SPOTIPY_CLIENT_ID'] = client_id
 os.environ['SPOTIPY_CLIENT_SECRET'] = client_secret
 os.environ['SPOTIPY_REDIRECT_URI'] = redirect_uri
 
+
 class SpotifyApi(object):
-    
+
     client_id = os.environ['SPOTIPY_CLIENT_ID']
     client_secret = os.environ['SPOTIPY_CLIENT_SECRET']
-    redirect_uri = os.environ['SPOTIPY_REDIRECT_URI']  
+    redirect_uri = os.environ['SPOTIPY_REDIRECT_URI']
     _id = oauth2.SpotifyOAuth(client_id, client_secret, redirect_uri)
-    client_test= oauth2.SpotifyClientCredentials()
-    def generate_token(self, user_code):
-        url="https://accounts.spotify.com/api/token"
-        data={
-            'grant_type': 'client_credentials',
-            #'grant_type': 'authorization_code',
-            'code':user_code,
-            'redirect_uri':"http://localhost:8080/tester"
-        }    
-    
+    client_test = oauth2.SpotifyClientCredentials()
 
-        response=requests.post(url, data=data, auth = (self.client_id, self.client_secret)) 
-    
+    def generate_token(self, user_code):
+        url = "https://accounts.spotify.com/api/token"
+        data = {
+            'grant_type': 'client_credentials',
+            # 'grant_type': 'authorization_code',
+            'code': user_code,
+            'redirect_uri': "http://localhost:8080/tester"
+        }
+
+        response = requests.post(url, data=data, auth=(
+            self.client_id, self.client_secret))
 
         return response.json()
+
     def get_spotify_toolbox(self, requesting):
-        #auth_obj = oauth2.SpotifyOAuth(client_id, client_secret, redirect_uri):
+        # auth_obj = oauth2.SpotifyOAuth(client_id, client_secret, redirect_uri):
         if 'code' in requesting.args:
             code = requesting.args.get('code')
-        
+
         elif 'error' in request.args:
             code = requesting.args.get('error')
             raise code
         else:
             code = "This was unexpected, where my queries at?"
 
-        #token=self._id.get_access_token(code)
-        #print("----------------token-----------------")
-                #print(token)
+        # token=self._id.get_access_token(code)
+        # print("----------------token-----------------")
+            # print(token)
 
         token = self.generate_token(code)
 
         token = token['access_token']
         #spotify_toolbox = spotipy.Spotify(client_credentials_manager=self._id, auth=token)
 
-        spotify_toolbox = spotipy.Spotify(client_credentials_manager=self.client_test)
+        spotify_toolbox = spotipy.Spotify(
+            client_credentials_manager=self.client_test)
         print("-------------------------------------")
         return spotify_toolbox
 
 
-
-
-
-
 @app.route('/login')
-def login():    
+def login():
     toolbox = SpotifyApi().get_spotify_toolbox(request)
     return "hi"
 
-    
 
-@app.route('/search', methods = ['GET', 'POST'])
+def build_query(content):
+    temp = str(content)
+    q = ""
+    for char in temp:
+        if char is "'" or char is "{" or char is "}":
+            pass
+        else:
+            q += char
+    return q
+
+
+def build_offset(offset, nudge, results):
+    print("++++build_OFFSET++++")
+    maxi = results["artists"]["total"]
+    offset = (int(maxi)*(int(offset)/100))+int(nudge)
+    return round(offset)
+
+@app.route('/search', methods=['GET', 'POST'])
 def search():
     toolbox = SpotifyApi().get_spotify_toolbox(request)
     if request.is_json:
         content = request.get_json()
-        print(content)
+        offset = content.pop("offset")
+        nudge = content.pop("nudge")
         toolbox = SpotifyApi().get_spotify_toolbox(request)
-        results = toolbox.search(q="genre: "+content.get('genre'),limit=5,offset=1,type="artist",market="IE")
-        return json.dumps(results)
+        query = build_query(content)
+        results = toolbox.search(
+            q=query, limit=1, offset=0, type="artist", market="IE")
+
+
+        offset = build_offset(offset, nudge, results)
+        results = toolbox.search(
+            q=query, limit=5, offset=offset, type="artist", market="IE")
         return jsonify(results)
 
     return("NO JSON OBJ IN POST")
 
 
-
-if __name__=='__main__':
-
-    print (SpotifyApi.client_id)
-    """spotipy.client.SpotifyOAuth(    
-        client_id=sys.argv[1],
-        client_secret=sys.argv[2] ,
-        redirect_uri='http://localhost:8080/tester')
-    """
-
-    """
-    auth_obj = oauth2.SpotifyOAuth(client_id, client_secret, redirect_uri):
-    token=auth_obj.get_access_token(code)
-
-    #client_credentials_manager = oauth2.SpotifyClientCredentials()
-    spotify_toolbox = spotipy.Spotify(client_credentials_manager=auth_obj, auth=token)
-    """
-    
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
-
