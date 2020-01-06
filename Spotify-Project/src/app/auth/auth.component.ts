98587a3f58ae2e7b4b2edcf157129b387443ecf4
Andrewk129 <andrewk129@gmail.com>
2020-01-06 12:11:57 +0000

    Removed console/Commented code

--- Spotify-Project/src/app/auth/auth.component.ts
+++ Spotify-Project/src/app/auth/auth.component.ts
@@ -25,7 +25,6 @@ export class AuthComponent implements OnInit{
                 console.log(this.userInfo);
             })
         }
-        console.log("init");  
     }
 
     Authorize() : void {
